014bf95ede24318b1628548583bcdee7a43639a9
jale0023-gif <jale0023@student.monash.edu>
2025-10-01 02:45:32 +1000

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,9 +0,0 @@
-// vega_lite_vis.js
-
-vegaEmbed("#vis", "population_map.vg.json", { actions: false })
-  .then((result) => {
-    console.log("Map loaded successfully");
-  })
-  .catch((error) => {
-    console.error("Error embedding map:", error);
-  });
