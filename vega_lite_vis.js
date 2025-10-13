f36abd0a44dc93e21b73cf94815380a354b8c489
jale0023-gif <jale0023@student.monash.edu>
2025-10-14 09:27:49 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -6,6 +6,21 @@ vegaEmbed("#gambling_map", vg_1)
   })
   .catch(console.error);
 
-vegaEmbed("#line_chart", "gambling_expenditure_multiline.vg.json", {actions: false});
+
+vegaEmbed("#line_chart", "gambling_expenditure_multiline.vg.json", { actions: false })
+  .then(function(result) {
+  })
+  .catch(console.error);
 
 
+vegaEmbed("#global_gambling_chart", "global_gambling_revenue.vg.json", { actions: false })
+  .then(function(result) {
+  })
+  .catch(console.error);
+
+vegaEmbed("#age_gambling_chart", "age_vs_gambling_type.vg.json", {actions: false})
+  .then(function(result) {})
+  .catch(console.error);
+
+vegaEmbed("#sankey_chart", "gambling_activity_risk.vg.json", {actions: false})
+  .catch(console.error);
